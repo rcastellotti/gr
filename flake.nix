0af046652b0962661c60a7ca9fa1e4ddfa28b702
rcastellotti <me@rcastellotti.dev>
2023-05-29 16:02:56 +0000

    stuff

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.ninja
+        ];
+      };
+    }
+  );
+}
