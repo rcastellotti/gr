c0bc6c7e1671a881d98a653701d84d61ec66dcb8
rcastellotti <me@rcastellotti.dev>
2023-07-07 14:45:13 +0000

    updated fio

--- fio.py
+++ fio.py
@@ -13,37 +13,43 @@ def run_fio_test(machine, filename, iodepth, rw, bs, numjobs):
         "--numjobs=" + str(numjobs),
         "--runtime=30",
         "--output-format=json",
-        "--direct",
+        "--direct=1",
         "--output=" + "direct-"+name + ".json",
     ]
-    subprocess.run(command)
-    command = [
-        "fio",
-        "--name=" + name,
-        "--filename=" + filename,
-        "--iodepth=" + str(iodepth),
-        "--rw=" + rw,
-        "--size=1G",
-        "--bs=" + bs,
-        "--numjobs=" + str(numjobs),
-        "--runtime=30",
-        "--output-format=json",
-        "--output="+name + ".json",
-    ]
+    # subprocess.run(command)
+    # command = [
+    #     "fio",
+    #     "--name=" + name,
+    #     "--filename=" + filename,
+    #     "--iodepth=" + str(iodepth),
+    #     "--rw=" + rw,
+    #     "--size=1G",
+    #     "--bs=" + bs,
+    #     "--numjobs=" + str(numjobs),
+    #     "--runtime=30",
+    #     "--output-format=json",
+    #     "--output=" + "indirect-"+name + ".json",
+    # ]
     subprocess.run(command)
 
 
 # Test configurations
 tests = [
-    {"iodepth": 128,"rw": "randread","bs": "4k","numjobs": 4},
-    {"iodepth": 128,"rw": "randwrite","bs": "4k","numjobs": 4},
-    {"iodepth": 128,"rw": "read","bs": "128k","numjobs": 4},
-    {"iodepth": 128,"rw": "write","bs": "128k","numjobs": 4},   
-    {"iodepth": 1,"rw": "randread","bs": "4k","numjobs": 4},
-    {"iodepth": 1,"rw": "randwrite","bs": "4k","numjobs": 4,},
+    # {"iodepth": 128,"rw": "randread","bs": "4k","numjobs": 4},
+    # {"iodepth": 128,"rw": "randwrite","bs": "4k","numjobs": 4},
+    # {"iodepth": 128,"rw": "read","bs": "128k","numjobs": 4},
+    # {"iodepth": 128,"rw": "write","bs": "128k","numjobs": 4},   
+    {"iodepth": 1,"rw": "randread","bs": "4k","numjobs": 1},
+    {"iodepth": 1,"rw": "randwrite","bs": "4k","numjobs": 1},
     {"iodepth": 1,"rw": "read","bs": "4k","numjobs": 1},
     {"iodepth": 1,"rw": "write","bs": "4k","numjobs": 1},
 ]
 
 for test in tests:
     run_fio_test(**test, machine="baremetal", filename="/mnt/a")
+    run_fio_test(**test, machine="sev-scsi", filename="/mnt/a")
+    run_fio_test(**test, machine="sev-blk", filename="/mnt/a")
+    run_fio_test(**test, machine="sev-nvme", filename="/mnt/a")
+    run_fio_test(**test, machine="nosev-scsi", filename="/mnt/a")
+    run_fio_test(**test, machine="nosev-blk", filename="/mnt/a")
+    run_fio_test(**test, machine="nosev-nvme", filename="/mnt/a")
