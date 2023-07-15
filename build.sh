2f34bb722099322b83f8305060073def3ccfb44c
rcastellotti <me@rcastellotti.dev>
2023-07-15 22:08:58 +0000

    stuff

--- build.sh
+++ build.sh
@@ -1,111 +1,37 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
-set -xe
-KERNEL_GIT_URL="https://github.com/AMDESE/linux.git"
-KERNEL_HOST_BRANCH="snp-host-latest"
-QEMU_GIT_URL=""
-
-SCRIPT_DIR="$(dirname $0)"
-[ -e /etc/os-release ] && . /etc/os-release
-
-run_cmd()
-{
-	echo "$*"
-
-	eval "$*" || {
-		echo "ERROR: $*"
-		exit 1
-	}
-}
-
-build_install_ovmf()
-{
-	DEST="$1"
-
-	GCC_VERSION=$(gcc -v 2>&1 | tail -1 | awk '{print $3}')
-	GCC_MAJOR=$(echo $GCC_VERSION | awk -F . '{print $1}')~
-	GCC_MINOR=$(echo $GCC_VERSION | awk -F . '{print $2}')
-	if [ "$GCC_MAJOR" == "4" ]; then
-		GCCVERS="GCC${GCC_MAJOR}${GCC_MINOR}"
-	else
-		GCCVERS="GCC5"
-	fi
-
-	# captures all the OVMF debug messages on qemu serial log. remove -DDEBUG_ON_SERIAL_PORT to disable it.
-	BUILD_CMD="nice build -q --cmd-len=64436 -DDEBUG_ON_SERIAL_PORT -n $(getconf _NPROCESSORS_ONLN) ${GCCVERS:+-t $GCCVERS} -a X64 -p OvmfPkg/OvmfPkgX64.dsc"
-
-	[ -d ovmf ] || {
-		run_cmd git clone --single-branch -b ${OVMF_BRANCH} ${OVMF_GIT_URL} ovmf
-
-		pushd ovmf >/dev/null
-			run_cmd git submodule update --init --recursive
-		popd >/dev/null
-	}
+build_install_ovmf(){
+	GCCVERS="GCC5"
+	git clone --single-branch -b snp-latest https://github.com/AMDESE/ovmf
+	pushd ovmf >/dev/null
+		git submodule update --init --recursive
+	popd >/dev/null
 
 	pushd ovmf >/dev/null
-		run_cmd git apply --ignore-space-change --ignore-whitespace ../ovmf.patch
-		run_cmd make -C BaseTools
+		git apply --ignore-space-change --ignore-whitespace ../ovmf.patch
+		make -C BaseTools
 		. ./edksetup.sh --reconfig
-		run_cmd $BUILD_CMD
+		nice build -q --cmd-len=64436 -DDEBUG_ON_SERIAL_PORT -n $(getconf _NPROCESSORS_ONLN) ${GCCVERS:+-t $GCCVERS} -a X64 -p OvmfPkg/OvmfPkgX64.dsc
 
-		mkdir -p $DEST
-		run_cmd cp -f Build/OvmfX64/DEBUG_$GCCVERS/FV/OVMF_CODE.fd $DEST
-		run_cmd cp -f Build/OvmfX64/DEBUG_$GCCVERS/FV/OVMF_VARS.fd $DEST
+		mkdir -p ../usr/local/share/qemu
+		cp -f Build/OvmfX64/DEBUG_$GCCVERS/FV/OVMF_CODE.fd ../usr/local/share/qemu
+		cp -f Build/OvmfX64/DEBUG_$GCCVERS/FV/OVMF_VARS.fd ../usr/local/share/qemu
 	popd >/dev/null
 }
 
-build_install_qemu()
-{
-	git clone --single-branch -b "snp-latest" https://github.com/AMDESE/qemu.git qemu
+build_install_qemu(){
+	git clone --single-branch -b "snp-latest" https://github.com/AMDESE/qemu.git
 	MAKE="make -j $(getconf _NPROCESSORS_ONLN)"
 	pushd qemu >/dev/null
 		git apply ../qemu.patch
+		git submodule init
+		git submodule update --recursive
 		./configure --target-list=x86_64-softmmu --prefix=/mnt/roberto/gr/usr/local --disable-werror
 		$MAKE
 		$MAKE install
 	popd >/dev/null
 }
 
-INSTALL_DIR="`pwd`/usr/local"
-
-while [ -n "$1" ]; do
-	case "$1" in
-	--install)
-		[ -z "$2" ] && usage
-		INSTALL_DIR="$2"
-		shift; shift
-		;;
-	-h|--help)
-		usage
-		;;
-	-*|--*)
-		echo "Unsupported option: [$1]"
-		;;
-	*)
-		break
-		;;
-	esac
-done
-
-mkdir -p $INSTALL_DIR
-IDIR=$INSTALL_DIR
-INSTALL_DIR=$(readlink -e $INSTALL_DIR)
-[ -n "$INSTALL_DIR" -a -d "$INSTALL_DIR" ] || {
-	echo "Installation directory [$IDIR] does not exist, exiting"
-	exit 1
-}
-
-if [ -z "$1" ]; then
-	build_install_qemu "$INSTALL_DIR"
-	build_install_ovmf "$INSTALL_DIR/share/qemu"
-else
-	case "$1" in
-	qemu)
-		build_install_qemu "$INSTALL_DIR"
-		;;
-	ovmf)
-		build_install_ovmf "$INSTALL_DIR/share/qemu"
-		;;
-	esac
-fi
+build_install_qemu
+build_install_ovmf
--- nosev.sh
+++ nosev.sh
@@ -19,39 +19,6 @@
 
 
 ## virtio-blk:
-# ./usr/local/bin/qemu-system-x86_64 \
-#     -enable-kvm \
-#     -cpu EPYC-v4,host-phys-bits=true \
-#     -smp 16 \
-#     -m 16G \
-#     -machine type=q35 \
-#     -drive if=pflash,format=raw,unit=0,file=/mnt/roberto/gr/OVMF_files/OVMF_CODE_nosev.fd,readonly=on \
-#     -drive if=pflash,format=raw,unit=1,file=/mnt/roberto/gr/OVMF_files/OVMF_VARS_nosev.fd \
-#     -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
-#     -drive file=nosev.img,if=none,id=drive0,format=raw \
-#     -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=4 \
-#     -nographic \
-#     -device virtio-net-pci,netdev=net0 \
-#     -netdev user,id=net0,hostfwd=tcp::2223-:22
-
-    # ubuntu@nosev:~$ lsblk
-    # NAME    MAJ:MIN RM   SIZE RO TYPE MOUNTPOINTS
-    # loop0     7:0    0  73.8M  1 loop /snap/core22/750
-    # loop1     7:1    0 170.1M  1 loop /snap/lxd/24918
-    # loop2     7:2    0  53.3M  1 loop /snap/snapd/19361
-    # loop3     7:3    0  53.3M  1 loop /snap/snapd/19457
-    # loop4     7:4    0  73.9M  1 loop /snap/core22/766
-    # loop5     7:5    0 173.5M  1 loop /snap/lxd/25112
-    # sr0      11:0    1   366K  0 rom  
-    # sr1      11:1    1  1024M  0 rom  
-    # vda     252:0    0  23.5G  0 disk 
-    # ├─vda1  252:1    0  23.4G  0 part /
-    # ├─vda14 252:14   0     4M  0 part 
-    # └─vda15 252:15   0   106M  0 part /boot/efi
-
-
-
-## nvme
 ./usr/local/bin/qemu-system-x86_64 \
     -enable-kvm \
     -cpu EPYC-v4,host-phys-bits=true \
@@ -61,25 +28,24 @@
     -drive if=pflash,format=raw,unit=0,file=/mnt/roberto/gr/OVMF_files/OVMF_CODE_nosev.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=/mnt/roberto/gr/OVMF_files/OVMF_VARS_nosev.fd \
     -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
-    -drive file=nosev.img,if=none,id=disk0,format=raw \
-    -device nvme,serial=cafebabe,drive=disk0 \
+    -drive file=nosev.img,if=none,id=drive0,format=raw \
+    -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=4 \
     -nographic \
     -device virtio-net-pci,netdev=net0 \
     -netdev user,id=net0,hostfwd=tcp::2223-:22
 
-    # ubuntu@nosev:~$ lsblk
-    # NAME         MAJ:MIN RM   SIZE RO TYPE MOUNTPOINTS
-    # loop0          7:0    0  73.8M  1 loop /snap/core22/750
-    # loop1          7:1    0  73.9M  1 loop /snap/core22/766
-    # loop2          7:2    0 170.1M  1 loop /snap/lxd/24918
-    # loop3          7:3    0 173.5M  1 loop /snap/lxd/25112
-    # loop4          7:4    0  53.3M  1 loop /snap/snapd/19361
-    # loop5          7:5    0  53.3M  1 loop /snap/snapd/19457
-    # sr0           11:0    1   366K  0 rom  
-    # sr1           11:1    1  1024M  0 rom  
-    # nvme0n1      259:0    0  23.5G  0 disk 
-    # ├─nvme0n1p1  259:1    0  23.4G  0 part /
-    # ├─nvme0n1p14 259:2    0     4M  0 part 
-    # └─nvme0n1p15 259:3    0   106M  0 part /boot/efi
-
-
+## nvme
+# ./usr/local/bin/qemu-system-x86_64 \
+#     -enable-kvm \
+#     -cpu EPYC-v4,host-phys-bits=true \
+#     -smp 16 \
+#     -m 16G \
+#     -machine type=q35 \
+#     -drive if=pflash,format=raw,unit=0,file=/mnt/roberto/gr/OVMF_files/OVMF_CODE_nosev.fd,readonly=on \
+#     -drive if=pflash,format=raw,unit=1,file=/mnt/roberto/gr/OVMF_files/OVMF_VARS_nosev.fd \
+#     -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
+#     -drive file=nosev.img,if=none,id=disk0,format=raw \
+#     -device nvme,serial=cafebabe,drive=disk0 \
+#     -nographic \
+#     -device virtio-net-pci,netdev=net0 \
+#     -netdev user,id=net0,hostfwd=tcp::2223-:22
