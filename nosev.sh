673e9db4f5583aea86dd7d2d620fc8b19c279eb1
rcastellotti <me@rcastellotti.dev>
2023-07-18 12:19:48 +0000

    updates

--- nosev.sh
+++ nosev.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-# virtio-scsi
+set -Eeo pipefail
+
+if [ $# -lt 1 ]; then
+    echo "usage: ./nosev.sh <device_type>"
+    exit 1
+fi
+
+[ ! -e kinetic-server-cloudimg-amd64.img ] && wget https://cloud-images.ubuntu.com/kinetic/current/kinetic-server-cloudimg-amd64.img
+mkdir -p OVMF_files
+
+# prepare the nosev machine
+rm -f nosev.img
+qemu-img convert kinetic-server-cloudimg-amd64.img nosev.img
+qemu-img resize nosev.img +20G
+rm -f cloud-config-nosev.iso
+
+
+if [ "$1" == "blk" ]; then
+    device_type="virtio-blk-pci,drive=disk0,id=virtblk0,num-queues=4"
+elif [ "$1"=="nvme" ]; then
+    device_type="nvme,serial=cafebabe,drive=disk0"
+elif [ "$1"=="scsi" ]; then
+    device_type="virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true"
+fi
+sed -i "s/- \[temp\]/- \[sudo, bash, \/run\/fio.sh, nosev, "$1"\]/" ./config/cloud-config-nosev.yml
+sudo cloud-localds cloud-config-nosev.iso config/cloud-config-nosev.yml
+
 ./usr/local/bin/qemu-system-x86_64 \
     -enable-kvm \
     -cpu EPYC-v4,host-phys-bits=true \
@@ -11,40 +37,8 @@
     -drive if=pflash,format=raw,unit=1,file=/mnt/roberto/gr/OVMF_files/OVMF_VARS_nosev.fd \
     -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
     -drive file=nosev.img,if=none,id=disk0,format=raw \
-    -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true \
-    -device scsi-hd,drive=disk0 \
+    -device $device_type \
     -nographic \
     -device virtio-net-pci,netdev=net0 \
     -netdev user,id=net0,hostfwd=tcp::2223-:22
 
-## virtio-blk:
-# ./usr/local/bin/qemu-system-x86_64 \
-#     -enable-kvm \
-#     -cpu EPYC-v4,host-phys-bits=true \
-#     -smp 16 \
-#     -m 16G \
-#     -machine type=q35 \
-#     -drive if=pflash,format=raw,unit=0,file=/mnt/roberto/gr/OVMF_files/OVMF_CODE_nosev.fd,readonly=on \
-#     -drive if=pflash,format=raw,unit=1,file=/mnt/roberto/gr/OVMF_files/OVMF_VARS_nosev.fd \
-#     -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
-#     -drive file=nosev.img,if=none,id=drive0,format=raw \
-#     -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=4 \
-#     -nographic \
-#     -device virtio-net-pci,netdev=net0 \
-#     -netdev user,id=net0,hostfwd=tcp::2223-:22
-
-## nvme
-# ./usr/local/bin/qemu-system-x86_64 \
-#     -enable-kvm \
-#     -cpu EPYC-v4,host-phys-bits=true \
-#     -smp 16 \
-#     -m 16G \
-#     -machine type=q35 \
-#     -drive if=pflash,format=raw,unit=0,file=/mnt/roberto/gr/OVMF_files/OVMF_CODE_nosev.fd,readonly=on \
-#     -drive if=pflash,format=raw,unit=1,file=/mnt/roberto/gr/OVMF_files/OVMF_VARS_nosev.fd \
-#     -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
-#     -drive file=nosev.img,if=none,id=disk0,format=raw \
-#     -device nvme,serial=cafebabe,drive=disk0 \
-#     -nographic \
-#     -device virtio-net-pci,netdev=net0 \
-#     -netdev user,id=net0,hostfwd=tcp::2223-:22
--- sev.sh
+++ sev.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-./usr/local/bin/qemu-system-x86_64 -d guest_errors\
+set -Eeo pipefail
+
+if [ $# -lt 1 ]; then
+    echo "usage: ./sev.sh <device_type>"
+    exit 1
+fi
+
+[ ! -e kinetic-server-cloudimg-amd64.img ] && wget https://cloud-images.ubuntu.com/kinetic/current/kinetic-server-cloudimg-amd64.img
+mkdir -p OVMF_files
+
+# prepare the sev machine
+rm -f sev.img
+qemu-img convert kinetic-server-cloudimg-amd64.img sev.img
+qemu-img resize sev.img +20G
+rm -f cloud-config-sev.iso
+
+
+if [ "$1" == "blk" ]; then
+    device_type="virtio-blk-pci,drive=disk0,id=virtblk0,num-queues=4"
+elif [ "$1"=="nvme" ]; then
+    device_type="nvme,serial=cafebabe,drive=disk0"
+elif [ "$1"=="scsi" ]; then
+    device_type="virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true"
+fi
+sed -i "s/- \[temp\]/- \[sudo, bash, \/run\/fio.sh, sev, "$1"\]/" ./config/cloud-config-sev.yml
+sudo cloud-localds cloud-config-sev.iso config/cloud-config-sev.yml
+
+./usr/local/bin/qemu-system-x86_64 \
     -enable-kvm \
     -cpu EPYC-v4,host-phys-bits=true \
     -smp 16 \
@@ -11,42 +38,7 @@
     -drive if=pflash,format=raw,unit=1,file=OVMF_files/OVMF_VARS_sev.fd \
     -drive file=cloud-config-sev.iso,media=cdrom,index=0 \
     -drive file=sev.img,if=none,id=disk0,format=raw \
-    -device virtio-scsi,id=scsi0 \
-    -device scsi-hd,drive=disk0 \
+    -device $device_type \
     -nographic \
     -device virtio-net-pci,netdev=net0 \
-    -netdev user,id=net0,hostfwd=tcp::2222-:22
-
-## virtio-blk:
-# ./usr/local/bin/qemu-system-x86_64 \
-#     -enable-kvm \
-#     -cpu EPYC-v4,host-phys-bits=true \
-#     -smp 16 \
-#     -machine type=q35,confidential-guest-support=sev0,memory-backend=ram1,kvm-type=protected,vmport=off \
-#     -object memory-backend-memfd-private,id=ram1,size=16G,share=true \
-#     -object sev-snp-guest,id=sev0,policy=0x30000,cbitpos=51,reduced-phys-bits=1,init-flags=0,host-data=b2l3bmNvd3FuY21wbXA \
-#     -drive if=pflash,format=raw,unit=0,file=OVMF_files/OVMF_CODE_sev.fd,readonly=on \
-#     -drive if=pflash,format=raw,unit=1,file=OVMF_files/OVMF_VARS_sev.fd \
-#     -drive file=cloud-config-sev.iso,media=cdrom,index=0 \
-#     -drive file=sev.img,if=none,id=disk0,format=raw \
-#     -device virtio-blk-pci,drive=disk0,id=virtblk0,num-queues=4 \
-#     -nographic \
-#     -device virtio-net-pci,netdev=net0 \
-#     -netdev user,id=net0,hostfwd=tcp::2222-:22
-
-## nvme
-# ./usr/local/bin/qemu-system-x86_64 \
-#     -enable-kvm \
-#     -cpu EPYC-v4,host-phys-bits=true \
-#     -smp 16 \
-#     -machine type=q35,confidential-guest-support=sev0,memory-backend=ram1,kvm-type=protected,vmport=off \
-#     -object memory-backend-memfd-private,id=ram1,size=16G,share=true \
-#     -object sev-snp-guest,id=sev0,policy=0x30000,cbitpos=51,reduced-phys-bits=1,init-flags=0,host-data=b2l3bmNvd3FuY21wbXA \
-#     -drive if=pflash,format=raw,unit=0,file=OVMF_files/OVMF_CODE_sev.fd,readonly=on \
-#     -drive if=pflash,format=raw,unit=1,file=OVMF_files/OVMF_VARS_sev.fd \
-#     -drive file=cloud-config-sev.iso,media=cdrom,index=0 \
-#     -drive file=sev.img,if=none,id=disk0,format=raw \
-#     -device nvme,serial=cafebabe,drive=disk0 \
-#     -nographic \
-#     -device virtio-net-pci,netdev=net0 \
-#     -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+    -netdev user,id=net0,hostfwd=tcp::2222-:22 \
