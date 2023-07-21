921850abfea43ab5d269546baa85b3beffae8754
Roberto Castellotti <me@rcastellotti.dev>
2023-07-21 15:11:01 +0200

    sth

--- prepare.sh
+++ prepare.sh
@@ -8,13 +8,13 @@ mkdir -p OVMF_files
 # prepare the nosev machine
 qemu-img convert kinetic-server-cloudimg-amd64.img nosev.img
 qemu-img resize nosev.img +20G
-sudo cloud-localds cloud-config-nosev.iso config/cloud-config-nosev.yml
+sudo cloud-localds cloud-config-nosev.iso cloud-config-nosev.yml
 cp ./usr/fds/OVMF_CODE.fd ./OVMF_files/OVMF_CODE_nosev.fd
 cp ./usr/fds/OVMF_VARS.fd ./OVMF_files/OVMF_VARS_nosev.fd
 
 # prepare the sev machine
 qemu-img convert kinetic-server-cloudimg-amd64.img sev.img
 qemu-img resize sev.img +20G
-sudo cloud-localds cloud-config-sev.iso ./config/cloud-config-sev.yml
+sudo cloud-localds cloud-config-sev.iso cloud-config-sev.yml
 cp ./usr/fds/OVMF_CODE.fd ./OVMF_files/OVMF_CODE_sev.fd
 cp ./usr/fds/OVMF_VARS.fd ./OVMF_files/OVMF_VARS_sev.fd
